2c5ddf955ce51e6bf3bf07594effaf91c4400c72
shaynewanjiru <shayne.wanjiru@student.moringaschool.com>
2026-06-12 14:07:07 +0300

    Add initial work for coffee feature

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = input("The size of coffee available is either small, medium or large. your chosen size is: ")
+        self.price = int(input("Please enter the price of the coffee: "))
+
+    @property
+    def size(self):
+        return self._size
+        
+    @size.setter
+    def size(self, value):    
+        if value in ["small", "medium", "large"]:
+            self._size = value
+            print(f"You chose a {value} size coffee")
+        else:
+            print("size must be small, medium, or large") 
+            self._size = None
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
+
+
+
+
+        
+    
+    
